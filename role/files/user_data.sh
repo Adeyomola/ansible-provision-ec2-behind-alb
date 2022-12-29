9bee3f37627ae4759d6e1934e0b7a4a80eb6c051
Adeyomola <kayzeel@yahoo.com>
2022-12-29 06:09:54 +0000

    Adjusted bash script for display DNS name

--- role/files/user_data.sh
+++ role/files/user_data.sh
@@ -3,9 +3,7 @@ sudo apt update
 sudo apt install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
-sudo echo '<!DOCTYPE html>
+sudo echo "<!DOCTYPE html>
 <html lang="en">
-<body>' > /var/www/html/index.html
-sudo echo $(dnsdomainname -f) >> /var/www/html/index.html
-sudo echo '</body>
-</html>' >> /var/www/html/index.html
+<body> $(dnsdomainname -f) </body>
+</html>" > /var/www/html/index.html
