2b22b4b00e01d4b6440a8054012c81bb83e1f0c1
Adeyomola <kayzeel@yahoo.com>
2023-04-15 15:11:10 +0100

    Improved Reusability

--- role/files/AverageCPUPolicy.sh
+++ role/files/AverageCPUPolicy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 aws autoscaling put-scaling-policy --policy-name tt-cpu-scaling \
   --auto-scaling-group-name "BiServerASG" --policy-type TargetTrackingScaling \
-  --target-tracking-configuration file:///home/ubuntu/holiday_challenge/role/files/ttconfig.json
+  --target-tracking-configuration "file:////home/adeyomola/ansible-provision-ec2-behind-alb/role/files/ttconfig.json" --region "eu-west-1"
